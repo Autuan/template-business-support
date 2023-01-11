f2f04ff99567f51d76744c5e8b44c1361fc4312f
Autuan.Yu <yu@autuan.top>
2023-01-11 18:47:21 +0800

    init project

---
+++ src/main/java/top/autuan/templatebusinesssupport/TemplateBusinessSupportApplication.java
@@ -0,0 +1,13 @@
+package top.autuan.templatebusinesssupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBusinessSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateBusinessSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/autuan/templatebusinesssupport/TemplateBusinessSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package top.autuan.templatebusinesssupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBusinessSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
