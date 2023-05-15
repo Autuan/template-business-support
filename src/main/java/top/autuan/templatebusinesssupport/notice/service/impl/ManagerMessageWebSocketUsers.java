9b970685429f4d7b3653ca29cd9a3b31427978c5
Autuan.Yu <autuan.yu@gmail.com>
2023-05-15 21:50:01 +0800

    注释 WebSocket 相关

--- src/main/java/top/autuan/templatebusinesssupport/notice/service/impl/ManagerMessageWebSocketUsers.java
+++ src/main/java/top/autuan/templatebusinesssupport/notice/service/impl/ManagerMessageWebSocketUsers.java
@@ -2,7 +2,6 @@ package top.autuan.templatebusinesssupport.notice.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.websocket.Session;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
@@ -20,7 +19,7 @@ public class ManagerMessageWebSocketUsers {
     /**
      * 用户集
      */
-    private static Map<String, Session> USERS = new ConcurrentHashMap<>();
+//    private static Map<String, Session> USERS = new ConcurrentHashMap<>();
     private static Map<String, String> sessionUserMap = new ConcurrentHashMap<>();
 
     /**
@@ -29,11 +28,11 @@ public class ManagerMessageWebSocketUsers {
      * @param key     唯一键
      * @param session 用户信息
      */
-    public static void put(String key, Session session) {
-        String sessionId = session.getId();
-        sessionUserMap.put(sessionId, key);
-        USERS.put(key, session);
-    }
+//    public static void put(String key, Session session) {
+//        String sessionId = session.getId();
+//        sessionUserMap.put(sessionId, key);
+//        USERS.put(key, session);
+//    }
 
     /**
      * 移除用户
@@ -42,23 +41,23 @@ public class ManagerMessageWebSocketUsers {
      * @return 移除结果
      */
 
-    public static boolean remove(Session session) {
-        String key = null;
-        boolean flag = USERS.containsValue(session);
-        if (flag) {
-            Set<Map.Entry<String, Session>> entries = USERS.entrySet();
-            for (Map.Entry<String, Session> entry : entries) {
-                Session value = entry.getValue();
-                if (value.equals(session)) {
-                    key = entry.getKey();
-                    break;
-                }
-            }
-        } else {
-            return true;
-        }
-        return remove(key);
-    }
+//    public static boolean remove(Session session) {
+//        String key = null;
+//        boolean flag = USERS.containsValue(session);
+//        if (flag) {
+//            Set<Map.Entry<String, Session>> entries = USERS.entrySet();
+//            for (Map.Entry<String, Session> entry : entries) {
+//                Session value = entry.getValue();
+//                if (value.equals(session)) {
+//                    key = entry.getKey();
+//                    break;
+//                }
+//            }
+//        } else {
+//            return true;
+//        }
+//        return remove(key);
+//    }
 
     /**
      * 移出用户
@@ -67,14 +66,15 @@ public class ManagerMessageWebSocketUsers {
      */
     public static boolean remove(String sessionId) {
         String key = sessionUserMap.get(sessionId);
-        Session remove = USERS.remove(key);
-        if (remove != null) {
-            boolean containsValue = USERS.containsValue(remove);
-            sessionUserMap.remove(sessionId);
-            return containsValue;
-        } else {
-            return true;
-        }
+//        Session remove = USERS.remove(key);
+//        if (remove != null) {
+//            boolean containsValue = USERS.containsValue(remove);
+//            sessionUserMap.remove(sessionId);
+//            return containsValue;
+//        } else {
+//            return true;
+//        }
+        return false;
     }
 
     /**
@@ -82,9 +82,9 @@ public class ManagerMessageWebSocketUsers {
      *
      * @return 返回用户集合
      */
-    public static Map<String, Session> getUsers() {
-        return USERS;
-    }
+//    public static Map<String, Session> getUsers() {
+//        return USERS;
+//    }
 
     /**
      * 群发消息文本消息
@@ -93,14 +93,14 @@ public class ManagerMessageWebSocketUsers {
      */
     @Deprecated
     public static void broadcast(String message) {
-        Collection<Session> values = USERS.values();
-        for (Session value : values) {
-            sendMessageToUserByText(value, message);
-        }
+//        Collection<Session> values = USERS.values();
+//        for (Session value : values) {
+//            sendMessageToUserByText(value, message);
+//        }
     }
 
     public static void sendMessage(String username, String message) {
-        sendMessageToUserByText(USERS.get(username), message);
+//        sendMessageToUserByText(USERS.get(username), message);
     }
 
     /**
@@ -109,17 +109,17 @@ public class ManagerMessageWebSocketUsers {
      * @param session session
      * @param message  消息内容
      */
-    public static void sendMessageToUserByText(Session session, String message) {
-        if (session != null) {
-            try {
-                session.getBasicRemote().sendText(message);
-            } catch (IOException e) {
-                log.info("ManagerMessageWebSocketUsers -> sendMessageToUserByText -> IOException -> sessionId -> {} e -> {}",
-                        session.getId(),e);
-            }
-        } else {
-            log.info("ManagerMessageWebSocketUsers -> sendMessageToUserByText -> session offline -> sessionId -> {} ",
-                    session.getId());
-        }
-    }
+//    public static void sendMessageToUserByText(Session session, String message) {
+//        if (session != null) {
+//            try {
+//                session.getBasicRemote().sendText(message);
+//            } catch (IOException e) {
+//                log.info("ManagerMessageWebSocketUsers -> sendMessageToUserByText -> IOException -> sessionId -> {} e -> {}",
+//                        session.getId(),e);
+//            }
+//        } else {
+//            log.info("ManagerMessageWebSocketUsers -> sendMessageToUserByText -> session offline -> sessionId -> {} ",
+//                    session.getId());
+//        }
+//    }
 }
